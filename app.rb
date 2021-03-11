38dd50e5ec94e50fd82dd88de879c7c9ba034fc0
Maxine <maxinemeurer@yahoo.com>
2021-03-11 14:31:56 -0500

    commit tree e8f9cbd43ef2ad06bfd00a3d86df52ee17db81d6

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Maxine."
+    end
+
+    get '/hometown' do 
+        "My hometown is Union Beach."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Kings and Queens."
+    end
 end
